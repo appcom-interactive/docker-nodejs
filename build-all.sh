cc3117f4dc266fface1f44f3783cc8263fa5e719
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2017-06-08 15:02:01 +0200

    added multiple nodejs versions

---
+++ build-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+docker pull alpine:3.5
+
+docker build -f Dockerfile-6 -t appcom/nodejs:6 .
+docker build -f Dockerfile-7 -t appcom/nodejs:7 .
+docker build -f Dockerfile-8 -t appcom/nodejs:8 .
+docker tag appcom/nodejs:8 appcom/nodejs:latest
+docker tag appcom/nodejs:6 appcom/nodejs:lts
