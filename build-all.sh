6871bebf987e4391e9a05657919dd1dd3baf2f39
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2018-04-10 16:18:54 +0200

    Tag NodeJS 9 as latest

--- build-all.sh
+++ build-all.sh
@@ -6,5 +6,5 @@ docker pull alpine:3.7
 docker build -f Dockerfile-8 -t appcom/nodejs:8 .
 docker build -f Dockerfile-9 -t appcom/nodejs:9 .
 
-docker tag appcom/nodejs:8 appcom/nodejs:latest
+docker tag appcom/nodejs:9 appcom/nodejs:latest
 docker tag appcom/nodejs:8 appcom/nodejs:lts
