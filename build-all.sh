196b4ff2059f00e348834a7d4654956ba6941fd6
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2018-06-08 08:48:03 +0200

    Add debian-9 build

--- build-all.sh
+++ build-all.sh
@@ -2,9 +2,14 @@
 set -e
 
 docker pull alpine:3.7
+docker pull debian:stretch-20180426
 
+# alpine based
 docker build -f Dockerfile-8 -t appcom/nodejs:8 .
 docker build -f Dockerfile-9 -t appcom/nodejs:9 .
+
+# debian based
+docker build -f Dockerfile-debian-9 -t appcom/nodejs:debian-9 .
 
 docker tag appcom/nodejs:9 appcom/nodejs:latest
 docker tag appcom/nodejs:8 appcom/nodejs:lts
