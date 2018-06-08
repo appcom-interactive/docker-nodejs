4228eb9f554bdbd4dbe95a04e055d5e2d8db5711
Ralph-Gordon Paul <g.paul@appcom-interactive.de>
2018-06-08 11:47:25 +0200

    Add debian nodejs 10

--- build-all.sh
+++ build-all.sh
@@ -10,6 +10,7 @@ docker build -f Dockerfile-9 -t appcom/nodejs:9 .
 
 # debian based
 docker build -f Dockerfile-debian-9 -t appcom/nodejs:debian-9 .
+docker build -f Dockerfile-debian-10 -t appcom/nodejs:debian-10 .
 
 docker tag appcom/nodejs:9 appcom/nodejs:latest
 docker tag appcom/nodejs:8 appcom/nodejs:lts
